9a525d3141c2fdf6f1b75f9d3660ffd7cbebc38e
COMENTCAVA <123748754+COMENTCAVA@users.noreply.github.com>
2025-06-15 17:33:36 +0200

    up

--- main.py
+++ main.py
@@ -22,16 +22,14 @@ for type in types:
         title = entry.title
         t = type
         date = entry.published
+        link = entry.link
 
         url_cves = f"https://www.cert.ssi.gouv.fr/{type}/{id}/json/"
         response = requests.get(url_cves)
         data = response.json()
 
-
-        vendor=data["affected_systems"][0]["product"]["vendor"]["name"]
         ref_cves = data["cves"]
-        print("editeur:",vendor)
-        print("CVE référencés ", ref_cves)
+        #print("CVE référencés ", ref_cves)
         for ref in ref_cves:
             #on recupère le nom de chaque cve
             name = ref["name"]
@@ -42,8 +40,6 @@ for type in types:
             metrics = data["containers"]["cna"].get("metrics", [])
             cvss_score = "Non disponible"
             cvss_severity = "Non disponible"
-
-
             if metrics:
                 cvss_data = metrics[0].get("cvssV3_0")
                 if cvss_data:
@@ -56,82 +52,42 @@ for type in types:
 
             cwe = "Non disponible"
             cwe_desc = "Non disponible"
-            problemtype = data["containers"]["cna"].get("problemTypes", [])
+            problemtype = data["containers"]["cna"].get("problemTypes", {})
             if problemtype and "descriptions" in problemtype[0]:
                 cwe = problemtype[0]["descriptions"][0].get("cweId", "Non disponible")
                 cwe_desc = problemtype[0]["descriptions"][0].get("description", "Non disponible")
 
-            print("CVSS Score:", cvss_score)
-            print("Severity:", cvss_severity)
-            print("CWE ID:", cwe)
-            print("CWE Description:", cwe_desc)
+            affected = data["containers"]["cna"]["affected"]
+            for product in affected:
+                vendor = product["vendor"]
+                product_name = product["product"]
+                versions = [v["version"] for v in product["versions"] if v["status"] == "affected"]
 
-            url=f"https://api.first.org/data/v1/epss?cve={name}"
+            description = data["containers"]["cna"]["descriptions"][0]["value"]
+
+            url = f"https://api.first.org/data/v1/epss?cve={name}"
             response = requests.get(url)
             data = response.json()
-            epss=round(float(data["data"][0]["epss"]),3)
+            epss = round(float(data["data"][0]["epss"]), 3)
 
-        link=entry.link
-        description=entry.description
+            print("")
+            print("ID ANSSI:",id)
+            print("Titre ANSSI:",title)
+            print("Type:",t)
+            print("Date:",date)
+            print("CVE:", name)
+            print("CVSS:", cvss_score)
+            print("Base Severity:", cvss_severity)
+            print("CWE:", cwe_desc)
+            print("EPSS:", epss)
+            print("Lien:",link)
+            print("Description:",description)
+            print("Editeur:", vendor)
+            print("Produit:", product_name)
+            print("Versions affectées:", versions)
+            print("")
+            print("------------------------------")
 
-        print(entry)
-        print("Titre :", entry.title)
-        print("Description:", entry.description)
-        print("Lien :", entry.link)
-        print("Date :", entry.published)
-        print("Epss :", epss)
-
-#Etape 2  trouver les failles CVE dans chaque bulletin
-
-
-url = "https://www.cert.ssi.gouv.fr/alerte/CERTFR-2024-ALE-001/json/"
-response = requests.get(url)
-data = response.json()
-
-# Méthode 1 : directement via la clé "cves"
-ref_cves = data["cves"]
-print( "CVE référencés ", ref_cves)
-
-# Méthode 2 : avec une expression régulière (regex)
-cve_pattern = r"CVE-\d{4}-\d{4,7}"
-cve_list = list(set(re.findall(cve_pattern, str(data))))
-print("CVE trouvés :", cve_list)
-
-#Etape 3 compléter les infos de CVE grâce à des API
-
-# Exemple pour MITRE
-cve_id = "CVE-2023-24488"
-url = f"https://cveawg.mitre.org/api/cve/{cve_id}"
-response = requests.get(url)
-data = requests.get(url).json()
-
-description = data["containers"]["cna"]["descriptions"][0]["value"]
-cvss_score = data["containers"]["cna"]["metrics"][0]["cvssV3_0"]["baseScore"]
-cvss_severity = data["containers"]["cna"]["metrics"][0]["cvssV3_0"]["baseSeverity"]
-
-# Extraire les produits affectés
-
-cwe = "Non disponible"
-cwe_desc="Non disponible"
-problemtype = data["containers"]["cna"].get("problemTypes", {})
-if problemtype and "descriptions" in problemtype[0]:
-    cwe = problemtype[0]["descriptions"][0].get("cweId", "Non disponible")
-    cwe_desc=problemtype[0]["descriptions"][0].get("description", "Non disponible")
-
-# Extraire les produits affectés
-affected = data["containers"]["cna"]["affected"]
-for product in affected:
-    vendor = product["vendor"]
-    product_name = product["product"]
-    versions = [v["version"] for v in product["versions"] if v["status"] == "affected"]
-    print(f"Éditeur : {vendor}, Produit : {product_name}, Versions : {', '.join(versions)}")
-
-# Afficher les résultats
-print(f"CVE : {cve_id}")
-print(f"Description : {description}")
-print(f"Score CVSS : {cvss_score}")
-print(f"Type CWE : {cwe}")
-print(f"CWE Description : {cwe_desc}")
 
 #Etape 4 regrouper toutes les infos dans un tableau
 
